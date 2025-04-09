5138656c769bbde3892cf80073e73b1c598813a4
Lucaaasz07 <luc.pizarroh@duocuc.cl>
2025-04-09 09:27:41 -0400

    Add files via upload

---
+++ clase2.1.py
@@ -0,0 +1,50 @@
+
+
+#suma
+print("Suma")
+print ("    ")
+
+V01 = int(input("Ingresa un numero entero: "))
+V02 = float(input("Ingresa un numero decimal: "))
+
+#operacion
+R01= V01 + V02
+
+print(f"el resultado es {R01}")
+print ("    ")
+
+#resta
+
+print("Resta")
+print ("    ")
+V03 = int(input("Ingrese otro numero: "))
+V04 = int(input("Ingrese otro numero: "))
+
+R02 = V03 - V04
+
+print(f"El resultado es {R02}")
+print ("    ")
+
+#Multiplicacion 
+print("Multiplicacion")
+print ("    ")
+
+V05 = int(input("Ingrese otro numero: "))
+V06 = int(input("Ingrese otro numero: "))
+
+R03 = V05 * V06 
+
+print(f"El resultado es {R03}")
+print ("    ")
+
+#Division 
+print("Division")
+print ("    ")
+V07 = int(input("Ingrese otro numero: "))
+V08 = int(input("Ingrese otro numero: "))
+
+R04 = V07 / V08
+
+print(f"El resultado es {R04}")
+
+
